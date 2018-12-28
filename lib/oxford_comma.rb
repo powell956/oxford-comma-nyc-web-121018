af1d3080752c99be4ce9ae7eaf15b4682f1632a1
powell956 <powell956@gmail.com>
2018-12-28 02:42:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
     array.join(" and ")
   else
     last_word = array.pop
-    complete_phrase = array.join(", ") + "and #{last_word}"
+    complete_phrase = array.join(", ") + " and #{last_word}"
   end
 end
