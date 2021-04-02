284be3ac1657bf3e100499f85beb2dd3e05c27b9
powell956 <powell956@gmail.com>
2021-04-02 13:00:42 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,26 @@
+require 'pry'
+
+# def oxford_comma(array)
+#   if array.length == 1
+#     array[0]
+#   elsif array.length == 2
+#     array.join(" and ")
+#   else
+#     last_word = array.pop
+#     complete_phrase = array.join(", ") + ", and #{last_word}"
+#   end
+# end
+
 def oxford_comma(array)
+  last_word = ""
+
   if array.length == 1
     array[0]
   elsif array.length == 2
-    array.join(" and ")
-  else
+    array[0] + " and " + array[1]
+  elsif array.length >= 3
     last_word = array.pop
-    complete_phrase = array.join(", ") + ", and #{last_word}"
+    # binding.pry
+    array.join(", ") + ", and " + last_word
   end
 end
